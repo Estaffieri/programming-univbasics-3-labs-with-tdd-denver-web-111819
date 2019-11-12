df8d86cc52994d70b111591ca53f383b000a56a3
Estelle S <estellelareinne@gmail.com>
2019-11-12 01:57:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,8 @@ describe "./calculator.rb" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
     expect(first_number).to be_an(Integer).or be_a(Float)
 end
+
+  it "contains a local variable called second_number that is assigned to a number"do
+    second_number = get_variable_from_file('./calculator.rb',"second_number")
+    expect(second_number).to be_an(Integer).or be_a(Float)
+  end  
