6654c6e8a11514e2f44c8f3c4db0c0b0049bbf08
Estelle S <estellelareinne@gmail.com>
2019-11-12 01:58:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -9,4 +9,4 @@ end
     second_number = get_variable_from_file('./calculator.rb',"second_number")
     expect(second_number).to be_an(Integer).or be_a(Float)
     expect(second_number).not_to equal(0)
-  end  
+end  
