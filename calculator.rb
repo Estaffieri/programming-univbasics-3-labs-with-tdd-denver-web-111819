db5b8e2efc4f67d1d5c7b3fb27e778df442890e5
Estelle S <estellelareinne@gmail.com>
2019-11-12 01:57:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -8,4 +8,5 @@ end
   it "contains a local variable called second_number that is assigned to a number"do
     second_number = get_variable_from_file('./calculator.rb',"second_number")
     expect(second_number).to be_an(Integer).or be_a(Float)
+    expect(second_number).not_to equal(0)
   end  
